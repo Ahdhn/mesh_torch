6206bbd0295f39cf0e37d36f6c8205e1c5ff8a05
ahmed <ahmahmoud@ucdavis.edu>
2024-10-16 19:45:36 -0400

    using the Laplacian instead of iterating over individual vertices

--- smoothing.py
+++ smoothing.py
@@ -9,93 +9,97 @@ from meshplot import plot, subplot, interact
 
 meshplot.offline()
 
+root_folder = os.getcwd()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = 'cuda'
 print(f"Working on {device} device")
 
+use_dense = False
 
-def laplacian_smoothing_energy(vertices, adj_matrix):
+
+def create_sparse_adjacency_matrix(faces, n_vertices):
     """
-    Compute the Laplacian smoothing energy using the sparse adjacency matrix 
-    For each vertex, compute the average position of its neighbors 
+    Create the Laplacian graph i.e., the vertex adjacency graph as a sparse 
+    CSR tensor 
     """
-    local_energies = torch.zeros(
-        vertices.shape[0], device=vertices.device)
+    per_face_half_edges = torch.stack(
+        [faces[:, (1, 2)], faces[:, (2, 0)], faces[:, (0, 1)]], -2
+    )
+    half_edges = torch.flatten(per_face_half_edges, -3, -2)
 
-    for v in range(vertices.shape[0]):
-        # get the indices of neighbors from the adjacency matrix (row i)
-        start = adj_matrix.crow_indices()[v].item()
-        end = adj_matrix.crow_indices()[v+1].item()
-        neighbor_indices = adj_matrix.col_indices()[start:end]
-        neighbor_positions = vertices[neighbor_indices]
-        average_position = neighbor_positions.mean(dim=0)
-        local_energies[v] = torch.sum((vertices[v] - average_position) ** 2)
+    idx = half_edges
+    values = torch.ones(half_edges.shape[0], device=device)
+    diag_idx = idx[:, 0]
 
-    total_energy = local_energies.sum()
-    return total_energy
+    idx = torch.cat((idx.mT, torch.stack((diag_idx, diag_idx), dim=0)), dim=1)
+    values = torch.cat((-values, values))
+
+    adj_matrix = torch.sparse_coo_tensor(
+        idx, values, (n_vertices, n_vertices)).coalesce().to_sparse_csr()
+    adj_matrix.requires_grad_(True)
+    return adj_matrix
 
 
-def create_adjacency_matrix(V, F):
+def sparse_laplacian_smoothing_energy(vert, adj_matrix):
     """
-    Create an adjacency matrix in a sparse format
+    Compute the Laplacian energy, i.e., the sum of squared edge length using 
+    the sparse graph Laplacian. 
     """
-    num_vertices = len(V)
-    row_id = []
-    col_id = []
-    for face in F:
+    res = adj_matrix @ vert
+    energy = torch.sum(res ** 2)
+    return energy
+
+
+def create_dense_adjacency_matrix(faces, n_vertices):
+    """
+    Create the Laplacian graph i.e., the vertex adjacency graph as a dense tensor 
+    """
+    adj_matrix = torch.zeros((n_vertices, n_vertices), device=device)
+    for f in faces:
         for i in range(3):
-            for j in range(3):
-                if i != j:
-                    row_id.append(face[i])
-                    col_id.append(face[j])
-
-    # Create adjacency matrix indices as tensor
-    indices = torch.tensor([row_id, col_id], dtype=torch.long)
-
-    # Create a sparse adjacency matrix with values of 1 at each (i, j)
-    coo_adj_matrix = torch.sparse_coo_tensor(indices, torch.ones(
-        len(row_id)), (num_vertices, num_vertices), device=device)
-
-    coo_adj_matrix = coo_adj_matrix.coalesce()
-
-    csr_adj_matrix = coo_adj_matrix.to_sparse_csr()
-
-    return csr_adj_matrix
+            adj_matrix[f[i], f[(i + 1) % 3]] = -1
+            adj_matrix[f[i], f[i]] += 1
+    return adj_matrix
 
 
-root_folder = os.getcwd()
+def dense_laplacian_smoothing_energy(vertices, adj_matrix):
+    """
+    Compute the Laplacian energy, i.e., the sum of squared edge length using 
+    the dense graph Laplacian. 
+    """
+    res = torch.matmul(adj_matrix, vertices)
+    energy = torch.sum(res ** 2)
+    return energy
+
 
 V, F = igl.read_triangle_mesh(os.path.join(root_folder, "bunnyhead.obj"))
 plot(V, F, filename="mesh.html", shading={"wireframe": True})
 
+vert = torch.tensor(V, dtype=torch.float32, requires_grad=True, device=device)
+faces = torch.tensor(F, device=device)
 
-vert = torch.tensor(V,  requires_grad=True, device=device)
-adj_matrix = create_adjacency_matrix(V, F)
+adj_matrix = create_dense_adjacency_matrix(faces, len(
+    V)) if use_dense else create_sparse_adjacency_matrix(faces, len(V))
 
-# print(adj_matrix)
+print(adj_matrix)
 
 learning_rate = 0.005
 num_iterations = 100
 
 for iter in range(num_iterations):
-    # loss function
-    energy = laplacian_smoothing_energy(vert, adj_matrix)
-    # compute gradients
+    energy = dense_laplacian_smoothing_energy(
+        vert, adj_matrix) if use_dense else sparse_laplacian_smoothing_energy(vert, adj_matrix)
     energy.backward()
-    print(f"energy = {energy}")
-
+    
     with torch.no_grad():
-        # take a step
         vert -= learning_rate * vert.grad
-        # rest the gradients
         vert.grad.zero_()
-    
-    
-    #if iter % 10 == 0:
-    #    V = vert.detach().cpu().numpy()
-    #    plot(V, F, filename="mesh.html", shading={"wireframe": True})
-    #    igl.write_triangle_mesh(os.path.join(
-    #        root_folder + "\out", "iter_" + str(iter) + "_.obj"), V, F)
-    #    print(f"Iteration {iter}: Energy = {energy.item()}")
-    
+
+    if iter % 10 == 0:
+        V = vert.detach().cpu().numpy()
+        plot(V, F, filename="mesh.html", shading={"wireframe": True})
+        print(f"Iteration {iter}: Energy = {energy.item()}")
+
+
+# igl.write_triangle_mesh(os.path.join(root_folder + "\out", "iter_" + str(iter) + "_.obj"), V, F)
